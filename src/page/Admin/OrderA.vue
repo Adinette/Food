b9cdcc8caf80fc118a48f7a4bb13d411025e77a2
Aziz Chanou <azizchanou@gmail.com>
2023-04-27 23:27:17 +0100

    wip

--- src/page/Admin/OrderA.vue
+++ src/page/Admin/OrderA.vue
@@ -7,23 +7,23 @@
     </button></router-link
   >
 
-  <div v-for="categorie in categories" :key="categorie.id">
+  <div v-for="categorie in categories" :key="categorie?.id">
     <div>
       <div class="shadow-md col-span-1 p-10 bg-white w-full max-w-xl">
         <img
-          :src="categorie.image"
+          :src="categorie?.image"
           alt=""
           class="hover:scale-125 duration-300 w-32"
         />
         <div class="flex flex-col items-end space-y-2 text-center">
-          <span class="font-bold text-slate-700">{{ categorie.nom }}</span>
+          <span class="font-bold text-slate-700">{{ categorie?.nom }}</span>
           <button
             class="p-2 py-1 m-2 bg-green-700 row-span-2 hover:bg-slate-500 text-white text-xl font-medium rounded w-32"
           >
             Consulter categorie
           </button>
         </div>
-        <router-link to="add"
+        <router-link :to="'/edit/' + categorie?.id"
           ><button
             type="submit"
             id="btn_modifier_categorie"
