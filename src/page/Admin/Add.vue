ccde474f7323600bcb1a08fee1c5182585ab5370
Aziz Chanou <azizchanou@gmail.com>
2023-04-28 01:30:43 +0100

    wip

--- src/page/Admin/Add.vue
+++ src/page/Admin/Add.vue
@@ -25,11 +25,13 @@
           type="file"
           name="image"
           id="image"
+          @change="onFileSelected"
           class="rounded bg-white w-full p-2"
         />
       </div>
 
       <button
+        @click="submit"
         type="submit"
         class="p-2 py-1 bg-green-700 text-white text-xl font-medium rounded w-full"
       >
@@ -51,66 +53,82 @@ const categorie = ref({
 
 const route = useRoute();
 const id = ref(null);
-let edit;
+// Je t'avais deja dit de ne pas utiliser les elements du JS mais on dirait que tu as manger la pate avec.
+// let edit;
 id.value = route.params.id;
 
-document.addEventListener("DOMContentLoaded", function () {
-  let form = document.getElementById("formCat");
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let nom = document.getElementById("nom");
-    let image = document.getElementById("image");
-    if (nom.value == "" || image.value == "") {
-      alert("Tous les champs sont obligatoires");
-    } else if (edit != categorie?.id) {
-      try {
-        const toSend = new FormData();
-        toSend.append("categorie", JSON.stringify({ nom: nom.value }));
-        toSend.append("image", image.files[0]);
-
-        const result = request(
-          "categorie",
-          "POST",
-          { Authorization: localStorage.getItem("token") },
-          toSend,
-          false
-        );
-        result.then((data) => {});
-      } catch (error) {
-        console.log(error);
-      }
-    } else {
-      try {
-        const result = request(
-          "categorie",
-          "GET",
-          { Authorization: localStorage.getItem("token") },
-          null,
-          false
-        );
-        result.then((data) => {
-          console.log(id);
-        });
-      } catch (error) {
-        console.log(error);
-      }
-      try {
-        const result = request(
-          "categorie",
-          "PUT",
-          { Authorization: localStorage.getItem("token") },
-          null,
-          false
-        );
-        result.then((data) => {
-          alert("La catégorie a été mise à jour avec succès !");
-        });
-      } catch (error) {
-        console.log(error);
-      }
+//Ce code s'execute des que le composant est monte.
+onMounted(() => {
+  //si l'id existe, tu executes la requete de verification. Normalement tu dois mettre les valeurs drecuperer dans e formulaire mais je ne sais pas ce que ca contient donc
+  //c'est a toi de faire un log et de mettre les bonnes infos ou il faut3
+  if (id.value) {
+    try {
+      const result = request(
+        "categorie",
+        "GET",
+        { Authorization: localStorage.getItem("token") },
+        null,
+        false
+      );
+      result.then((data) => {
+        // en suposant que la donne renvoyer contient le nom directement00
+        //categorie.value.nom = data.nom
+        console.log(data);
+      });
+    } catch (error) {
+      console.log(error);
     }
-  });
+  }
 });
+
+const onFileSelected = (event) => {
+  categorie.value.image = event.target.files[0];
+  console.log(categorie.value.image);
+};
+
+const submit = () => {
+  //Tu verifie si les champs sont emplies
+  if (categorie.value.nom == "" || categorie.value.image == null) {
+    alert("Tous les champs sont obligatoires");
+  }
+
+  const toSend = new FormData();
+  toSend.append("categorie", JSON.stringify({ nom: categorie.value.nom }));
+  toSend.append("image", categorie.value.image);
+
+  //Si l'id est definie, tu execute cette requete
+  if (id.value) {
+    try {
+      const result = request(
+        "categorie",
+        "POST",
+        { Authorization: localStorage.getItem("token") },
+        toSend,
+        false
+      );
+      result.then((res) => {
+        console.log(res);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    try {
+      const result = request(
+        "categorie",
+        "PUT",
+        { Authorization: localStorage.getItem("token") },
+        toSend,
+        false
+      );
+      result.then((data) => {
+        alert("La catégorie a été mise à jour avec succès !");
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
 </script>
 
 <style></style>
